abc3a7df2a2401964118cf56ae68263f9991ee67
kamifuta <kamifuta010@outlook.jp>
2022-06-16 18:59:54 +0900

    ジャンプの実装

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody rigidbody;
 
     private Vector3 MoveVec => new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+    private bool PushedJumpButton => Input.GetKeyDown(KeyCode.Space);
+
+    private readonly Vector3 JumpPower = new Vector3(0, 10f, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,8 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         LookAtMoveDirectin();
+        if (PushedJumpButton)
+            Jump();
     }
 
     private void Move()
@@ -29,5 +34,10 @@ public class PlayerController : MonoBehaviour
     private void LookAtMoveDirectin()
     {
         transform.LookAt(transform.position + rigidbody.velocity);
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(JumpPower, ForceMode.Impulse);
     }
 }
